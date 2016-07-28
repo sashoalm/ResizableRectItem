620b63f28c03482a55343c6a930c0a93373c49ff
Alexander Almaleh <sashoalm@gmail.com>
2016-07-28 09:02:04 +0300

    Print the mouse position during press, move and release.

--- resizablerectitem.cpp
+++ resizablerectitem.cpp
@@ -1,7 +1,24 @@
 #include "resizablerectitem.h"
+#include <QDebug>
+#include <QGraphicsSceneMouseEvent>
 
 ResizableRectItem::ResizableRectItem(const QRectF &rect, QGraphicsItem *parent)
     : QGraphicsRectItem(rect, parent)
 {
 
+}
+
+void ResizableRectItem::mousePressEvent(QGraphicsSceneMouseEvent *event)
+{
+    qDebug() << "press" << event->pos() << event->lastPos() << event->scenePos() << event->lastScenePos();
+}
+
+void ResizableRectItem::mouseMoveEvent(QGraphicsSceneMouseEvent *event)
+{
+    qDebug() << "move" << event->pos() << event->lastPos() << event->scenePos() << event->lastScenePos();
+}
+
+void ResizableRectItem::mouseReleaseEvent(QGraphicsSceneMouseEvent *event)
+{
+    qDebug() << "release" << event->pos() << event->lastPos() << event->scenePos() << event->lastScenePos();
 }
--- resizablerectitem.h
+++ resizablerectitem.h
@@ -7,6 +7,11 @@ class ResizableRectItem : public QGraphicsRectItem
 {
 public:
     ResizableRectItem(const QRectF &rect, QGraphicsItem *parent = 0);
+
+private:
+    void mousePressEvent(QGraphicsSceneMouseEvent *event);
+    void mouseMoveEvent(QGraphicsSceneMouseEvent *event);
+    void mouseReleaseEvent(QGraphicsSceneMouseEvent *event);
 };
 
 #endif // RESIZABLERECTITEM_H
