cd6a064fc7acbb1ffc00ec704b1ce5b86d7b4dbb
Alexander Almaleh <sashoalm@gmail.com>
2016-07-28 10:27:20 +0300

    Remove the debug logging.

--- resizablerectitem.cpp
+++ resizablerectitem.cpp
@@ -1,5 +1,4 @@
 #include "resizablerectitem.h"
-#include <QDebug>
 #include <QGraphicsSceneMouseEvent>
 
 ResizableRectItem::ResizableRectItem(const QRectF &rect, qreal resizablePart, QGraphicsItem *parent)
@@ -10,33 +9,26 @@ ResizableRectItem::ResizableRectItem(const QRectF &rect, qreal resizablePart, QG
 
 void ResizableRectItem::mousePressEvent(QGraphicsSceneMouseEvent *event)
 {
-    qDebug() << "press" << event->pos() << event->lastPos() << event->scenePos() << event->lastScenePos();
-
     qreal widthPart = resizablePart * rect().width() / 2;
     qreal heightPart = resizablePart * rect().height() / 2;
     QRectF innerRect = rect().adjusted(widthPart, heightPart, -widthPart, -heightPart);
-
-    qDebug() << rect().topLeft() << rect().bottomRight() << innerRect.topLeft() << innerRect.bottomRight();
 
     resizeDirections.left = event->pos().x() < innerRect.left();
     resizeDirections.right = event->pos().x() > innerRect.right();
     resizeDirections.top = event->pos().y() < innerRect.top();
     resizeDirections.bottom = event->pos().y() > innerRect.bottom();
 
-    qDebug() << resizeDirections.left << resizeDirections.top << resizeDirections.right << resizeDirections.bottom;
     Q_ASSERT(!(resizeDirections.left && resizeDirections.right));
     Q_ASSERT(!(resizeDirections.top && resizeDirections.bottom));
 }
 
 void ResizableRectItem::mouseMoveEvent(QGraphicsSceneMouseEvent *event)
 {
-    qDebug() << "move" << event->pos() << event->lastPos() << event->scenePos() << event->lastScenePos();
     resizeRect(event);
 }
 
 void ResizableRectItem::mouseReleaseEvent(QGraphicsSceneMouseEvent *event)
 {
-    qDebug() << "release" << event->pos() << event->lastPos() << event->scenePos() << event->lastScenePos();
     resizeRect(event);
 }
 
