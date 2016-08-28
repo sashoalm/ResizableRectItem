e049732827be604879f3a946037fee9e5a25dcb0
Alexander Almaleh <sashoalm@gmail.com>
2016-08-28 20:50:45 +0300

    getRect and setRect

--- pyscripting.cpp
+++ pyscripting.cpp
@@ -30,10 +30,29 @@ static PyObject* pythonPos(PyObject *self, PyObject *args)
     return Py_BuildValue("dd", pos.x(), pos.y());
 }
 
+static PyObject* pythonSetRect(PyObject *self, PyObject *args)
+{
+    Q_UNUSED(self);
+    double x, y, w, h;
+    PyArg_ParseTuple(args, "dddd", &x, &y, &w, &h);
+    pthis->setRect(x, y, w, h);
+    Py_RETURN_NONE;
+}
+
+static PyObject* pythonRect(PyObject *self, PyObject *args)
+{
+    Q_UNUSED(self);
+    Q_UNUSED(args);
+    QRectF r = pthis->rect();
+    return Py_BuildValue("dddd", r.x(), r.y(), r.width(), r.height());
+}
+
 static PyMethodDef methods[] = {
     { "setColor", pythonSetColor, METH_VARARGS, "set the color of the current rect item" },
     { "setPos", pythonSetPos, METH_VARARGS, "set the position of the current rect item" },
     { "pos", pythonPos, METH_NOARGS, "get the position of the current rect item" },
+    { "setRect", pythonSetRect, METH_VARARGS, "set the rectangle of the current rect item" },
+    { "rect", pythonRect, METH_NOARGS, "get the rectangle of the current rect item" },
     { 0, 0, 0, 0 }
 };
 
